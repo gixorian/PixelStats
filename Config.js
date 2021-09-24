121266ea76a34e4273abe159032793eea8ea080b
Gix <goranborovecki1@gmail.com>
2021-09-24 17:17:59 +0200

    Initial Commit

---
+++ Config.js
@@ -0,0 +1,12 @@
+const config = {
+  type: 'radar',
+  data: data,
+  options: {
+    responsive: false,
+    elements: {
+      line: {
+        borderWidth: 5
+      }
+    }
+  },
+};
---
+++ Setup.js
@@ -0,0 +1,41 @@
+const data = {
+  labels: [
+    'Hp',
+    'Attack',
+    'Defense',
+    'Sp. Attack',
+    'Sp. Defense',
+    'Speed'
+  ],
+  datasets: [{
+    label: 'Bulbasaur',
+    data: [45, 49, 49, 65, 65, 45],
+    fill: true,
+    backgroundColor: 'rgba(143, 227, 111, 0.4)',
+    borderColor: 'rgb(143, 227, 111)',
+    pointBackgroundColor: 'rgb(143, 227, 111)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(143, 227, 111)'
+  }, {
+    label: 'Charmander',
+    data: [39, 52, 43, 60, 50, 65],
+    fill: true,
+    backgroundColor: 'rgba(255, 99, 132, 0.4)',
+    borderColor: 'rgb(255, 99, 132)',
+    pointBackgroundColor: 'rgb(255, 99, 132)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(255, 99, 132)'
+  }, {
+    label: 'Squirtle',
+    data: [44, 48, 65, 50, 64, 43],
+    fill: true,
+    backgroundColor: 'rgba(111, 200, 227, 0.4)',
+    borderColor: 'rgb(111, 200, 227)',
+    pointBackgroundColor: 'rgb(111, 200, 227)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(111, 200, 227)'
+  }]
+};
