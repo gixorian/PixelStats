c397801608dee24377491a444a42483407a85f26
Gix <goranborovecki1@gmail.com>
2021-09-25 01:24:09 +0200

    Functional

--- Setup.js
+++ Setup.js
@@ -8,7 +8,7 @@ const data = {
     'Speed'
   ],
   datasets: [{
-    id: 1,
+    id: 0,
     label: 'Bulbasaur',
     data: [45, 49, 49, 65, 65, 45],
     fill: true,
@@ -18,27 +18,5 @@ const data = {
     pointBorderColor: '#fff',
     pointHoverBackgroundColor: '#fff',
     pointHoverBorderColor: 'rgb(143, 227, 111)'
-  }, {
-    id: 2,
-    label: 'Charmander',
-    data: [39, 52, 43, 60, 50, 65],
-    fill: true,
-    backgroundColor: 'rgba(255, 99, 132, 0.4)',
-    borderColor: 'rgb(255, 99, 132)',
-    pointBackgroundColor: 'rgb(255, 99, 132)',
-    pointBorderColor: '#fff',
-    pointHoverBackgroundColor: '#fff',
-    pointHoverBorderColor: 'rgb(255, 99, 132)'
-  }, {
-    id: 3,
-    label: 'Squirtle',
-    data: [44, 48, 65, 50, 64, 43],
-    fill: true,
-    backgroundColor: 'rgba(111, 200, 227, 0.4)',
-    borderColor: 'rgb(111, 200, 227)',
-    pointBackgroundColor: 'rgb(111, 200, 227)',
-    pointBorderColor: '#fff',
-    pointHoverBackgroundColor: '#fff',
-    pointHoverBorderColor: 'rgb(111, 200, 227)'
   }]
 };
