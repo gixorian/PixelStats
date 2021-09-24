ccda9329643f495ddc4c7dfcb12a41af856ccbe1
Gix <goranborovecki1@gmail.com>
2021-09-24 17:53:58 +0200

    weird

--- Config.js
+++ Config.js
@@ -2,10 +2,20 @@ const config = {
   type: 'radar',
   data: data,
   options: {
+    scales: {
+      r: {
+        max: 200,
+        min: 0,
+        ticks: {
+            stepSize: 50
+        }
+      }
+    },
     responsive: false,
     elements: {
       line: {
-        borderWidth: 5
+        tension: 10,
+        borderWidth: 3
       }
     }
   },
